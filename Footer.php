94cba201cd0e6f091c845cf811466dbd4c5ba323
Unalcan-Ustundag <133241515+Unalcan-Ustundag@users.noreply.github.com>
2023-12-05 14:12:14 +0100

    init

--- Footer.php
+++ Footer.php
@@ -1,40 +1,36 @@
 
-        <div class="Contact">
-            <h3>Contact</h3>
-            <p>[D66 Partijnaam]</p>
-            <p>[Adres]</p>
-            <p>[Telefoonnummer]</p>
-            <p>[E-mailadres]</p>
-        </div>
-
-        <div class="D66">
-            <h3>Wat vindt D66</h3>
-            <ul>
-                <li>Standpunten</li>
-                <li>Nieuws & Updates</li>
-                <li>Persberichten</li>
-            </ul>
-        </div>
-
-        <div class="Mensen">
-            <h3>Onze Mensen</h3>
-            <ul>
-                <li>Onze Leiders</li>
-                <li>Ons Team</li>
-                <li>Fractieleden</li>
-            </ul>
-        </div>
-
-        <div class="Meedoen">
-            <h3>Meedoen</h3>
-            <ul>
-                <li>Lid worden</li>
-                <li>Vrijwilligerswerk</li>
-                <li>Donaties</li>
-            </ul>
-        </div>
-</footer>
-    <div class="footer-bottom">
-        <p>&copy; <?php echo date("Y"); ?> D66. Alle rechten voorbehouden.</p>
-        <p><a href="#">Privacybeleid</a> | <a href="#">Algemene Voorwaarden</a></p>
-    </div>
+<div class="contact">
+    <h3>Contact</h3>
+        [D66 Partijnaam]<br>
+        [Adres]<br>
+        [Telefoonnummer]<br>
+        [E-mailadres]
+</div>
+<div class="D66">
+    <h3>Wat vindt D66</h3>
+    <ul>
+        <li>Standpunten</li>
+        <li>Nieuws & Updates</li>
+        <li>Persberichten</li>
+    </ul>
+</div>
+<div class="Mensen">
+    <h3>Onze Mensen</h3>
+    <ul>
+        <li>Onze Leiders</li>
+        <li>Ons Team</li>
+        <li>Fractieleden</li>
+    </ul>
+</div>
+<div class="Meedoen">
+    <h3>Meedoen</h3>
+    <ul>
+        <li>Lid worden</li>
+        <li>Vrijwilligerswerk</li>
+        <li>Donaties</li>
+    </ul>
+</div>    
+<div class="footer-bottom">
+    <p>&copy; <?php echo date("Y"); ?> D66. Alle rechten voorbehouden.</p>
+    <p><a href="#">Privacybeleid</a> | <a href="#">Algemene Voorwaarden</a></p>
+</div>
--- index.php
+++ index.php
@@ -3,12 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Footer.css">
     <link rel="stylesheet" href="header.css">
     <title>Document</title>
 </head>
 <body>
-<?php include 'Header.php';
-include 'Footer.php';
-?>
+    <header>
+        <?php include 'Header.php';?>
+    </header>
+    <main>
+        BLAH BLAHs
+    </main>
+    <footer>
+        <?php include 'Footer.php'; ?>
+    </footer>
 </body>
 </html>
