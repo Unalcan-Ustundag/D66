6ab20c0b3997768de7972b0ebf21a43d80ef1265
Unalcan-Ustundag <133241515+Unalcan-Ustundag@users.noreply.github.com>
2023-12-05 10:05:20 +0100

    init

---
+++ Footer.php
@@ -0,0 +1,41 @@
+  <footer>
+        <div>
+            <h3>Contact</h3>
+            <p>[D66 Partijnaam]</p>
+            <p>[Adres]</p>
+            <p>[Telefoonnummer]</p>
+            <p>[E-mailadres]</p>
+        </div>
+
+        <div>
+            <h3>Wat vindt D66</h3>
+            <ul>
+                <li>Standpunten</li>
+                <li>Nieuws & Updates</li>
+                <li>Persberichten</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Onze Mensen</h3>
+            <ul>
+                <li>Onze Leiders</li>
+                <li>Ons Team</li>
+                <li>Fractieleden</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Meedoen</h3>
+            <ul>
+                <li>Lid worden</li>
+                <li>Vrijwilligerswerk</li>
+                <li>Donaties</li>
+            </ul>
+        </div>
+    </footer>
+
+    <div>
+        <p>&copy; <?php echo date("Y"); ?> D66. Alle rechten voorbehouden.</p>
+        <p><a href="#">Privacybeleid</a> | <a href="#">Algemene Voorwaarden</a></p>
+    </div>
--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-<?php include 'header.php'
+<?php include 'Header.php';
+include 'Footer.php';
 ?>
 </body>
 </html>
