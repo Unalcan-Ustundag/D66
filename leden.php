79997c0bf27bdd67d3941311367662f1b68bb474
Unalcan-Ustundag <133241515+Unalcan-Ustundag@users.noreply.github.com>
2023-12-08 13:15:40 +0100

    init

--- leden.php
+++ leden.php
@@ -13,19 +13,23 @@
     <?php include 'Header.php';?>
 </header>
 <main class="teamleden">
-<div class="pagetitle">
-    <h1>Teamleden</h1>
-</div>  
-<div>
-        <h1 class="naam1">Rob Jetten</h1>
-    
-        <img class="robj" src="images/d66-mensen-rob-jetten-landscape-scaled-e1700736255492.png">
-        <section>
-        
-        <img class="sigridk" src="images/xACCZCoR_400x400.jpg"><h3 class="naam2">Sigrid Kaag</h3></img>
-        <h3 class="naam3">Jan Paternotte</h3>
-</setion>
-</div>
+    <div class="leden">
+        <div class="pagetitle">
+            <h1>Teamleden</h1>
+        </div>  
+        <div class="lidg">
+            <h1 class="naam1">Rob Jetten</h1>
+            <img class="robj" src="images/RobJetten-D66-Lijsttrekker-e1698062003680.webp">
+        </div>
+        <div class="lid1">
+        <h3 class="naam2">Sigrid Kaag</h3>
+        <img class="sigridk" src="images/xACCZCoR_400x400.jpg"></img>
+        </div>
+        <div class="lid2"></div>
+        <div class="lid3"></div>
+        <div class="lid4"></div>
+    </div>
+            
 </main>
 <footer>
     <?php include 'Footer.php'; ?>
