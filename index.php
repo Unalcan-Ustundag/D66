65437c1b8584a399b5fb32c9eb4c6b0996efd454
wesley191 <banaansaper2005@gmail.com>
2023-12-08 12:43:38 +0100

    klaar

--- index.php
+++ index.php
@@ -1,30 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <title>Document</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="Footer.css">
+<link rel="stylesheet" href="Header.css">
+<link rel="icon" href="images/D66_logo_(2019present).png">
+<title>D66 - Nieuwe energie voor</title>
 </head>
-
 <body>
-    <?php include 'header.php'
-    ?>
-    <div>
-        <div class="page1">
+<header>
+    <?php include 'Header.php';?>
+</header>
+<main>
+<div>
+    <div class="page1">
 
-            <img class="image1" src="images/d66-mensen-rob-jetten-landscape-scaled-e1700736255492.png">
-            <h2 class="steun">
-            Bedankt <br>
-            voor je <br>
-            steun!
-            </h2>
-            <embed class="embed" src="https://youtu.be/sRZbvAcY0Lc">
-        </div>
-        <div class="page2">
-        </div>
+        <img class="image1" src="images/d66-mensen-rob-jetten-landscape-scaled-e1700736255492.png">
+        <h2 class="steun">
+        Bedankt <br>
+        voor je <br>
+        steun!
+        </h2>
+        <img class="embed" src="images/Screenshot 2023-12-08 094015.png">
+        <h2 class="sluit">Sluit je aan bij Rob <br> Jetten</h2>
+        <p class="txt1">Sta op, bouw mee. Dit is het moment om aan <br> Nederland te laten zien dat er een <br> optimistisch alternatief is.</p>
+        <button class="lidbutton">→ Word lid van D66</button>
     </div>
+    <div class="page2">
+        <h2 class="klimaat">Klimaat, <br> natuur en <br> milieu</h2>
+        <p class="txt2">D66 wil ‘eerlijk delen in een schoon land’, zoals in <br> 1972 al op onze verkiezingsposter stond. We zetten <br> volop door om de doelstelling van 60 procent CO2- <br>reductie in 2030 te…</p>
+        <button class="plannenbutton">Onze plannen</button>
+        <img class="klimaatimg" src="images/klimaat-rob2.png">
+    </div>
+    <div class="page3">
+        <h2 class="nieuws">Laatste nieuws</h2>
+        <img class="nieuwsimage" src="images/YrJwASN9oX_sE0LUtAlv3nWyLQ8.jpg">
+        <p class="txt3">Bekijk hier het Laatste <br> nieuws over D66</p>
+        <button class="nieuwsbutton">Lees het nieuws</button>
+    </div>
+</div>
+</main>
+<footer>
+    <?php include 'Footer.php'; ?>
+</footer>
 </body>
 
 </html>
