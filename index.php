775a9506f70403110819c47a0a6c07b0ad96911a
wesley191 <banaansaper2005@gmail.com>
2023-12-11 10:06:58 +0100

    header - donations

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="Footer.css">
 <link rel="stylesheet" href="Header.css">
-<title>Document</title>
+<link rel="stylesheet" href="index.css">
+<link rel="icon" href="images/D66_logo_(2019present).png">
+<title>D66 - Nieuwe energie voor</title>
 </head>
 <body>
 <header>
@@ -22,9 +24,22 @@
         voor je <br>
         steun!
         </h2>
-        <embed class="embed" src="https://youtu.be/sRZbvAcY0Lc">
+        <img class="embed" src="images/Screenshot 2023-12-08 134518.png">
+        <h2 class="sluit">Sluit je aan bij Rob <br> Jetten</h2>
+        <p class="txt1">Sta op, bouw mee. Dit is het moment om aan <br> Nederland te laten zien dat er een <br> optimistisch alternatief is.</p>
+        <button onclick="location.href='donations.php'" class="lidbutton">→ Word lid van D66</button>
     </div>
     <div class="page2">
+        <h2 class="klimaat">Klimaat, <br> natuur en <br> milieu</h2>
+        <p class="txt2">D66 wil ‘eerlijk delen in een schoon land’, zoals in <br> 1972 al op onze verkiezingsposter stond. We zetten <br> volop door om de doelstelling van 60 procent CO2- <br>reductie in 2030 te…</p>
+        <button class="plannenbutton">Onze plannen</button>
+        <img class="klimaatimg" src="images/klimaat-rob2 (1).png">
+    </div>
+    <div class="page3">
+        <h2 class="nieuws">Laatste nieuws</h2>
+        <img class="nieuwsimage" src="images/download.jpg">
+        <p class="txt3">Bekijk hier het Laatste <br> nieuws over D66</p>
+        <button class="nieuwsbutton">Lees het nieuws</button>
     </div>
 </div>
 </main>
@@ -33,10 +48,4 @@
 </footer>
 </body>
 
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="index.css">
-<title>Document</title>
-</head>
 </html>
