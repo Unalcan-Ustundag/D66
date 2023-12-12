930505a6e8355c0911ac2a1a66e6a13bde95dbc4
Unalcan-Ustundag <133241515+Unalcan-Ustundag@users.noreply.github.com>
2023-12-12 15:17:10 +0100

    leden

--- leden.php
+++ leden.php
@@ -18,17 +18,53 @@
             <h1>Teamleden</h1>
         </div>  
         <a class="lidg" href="https://d66.nl/mensen/rob-jetten/">
-            <img class="robj" src="images/robjetten.jpg">
-            <div class="naam1">Rob Jetten<br><br><h2 class="info1">Partijleider, demissionair <br>minister voor Klimaat en <br> Energie</h2></div>
-
+            <div class="lid">
+                <img class="robj" src="images/robjetten.jpg">
+                <div class="naam1"><br>Rob Jetten<br><br><h2 class="info1">Partijleider, demissionair <br>minister voor Klimaat en <br> Energie</h2></div>
+            </div>
         </a>
         <a class="lid1" href="https://d66.nl/mensen/sigrid-kaag/">
-            <img class="sigridk" src="images/xACCZCoR_400x400.jpg"> 
-            <h1 class="naam2">Sigrid Kaag</h1>
+            <div class="lid">
+                <img class="sigridk" src="images/xACCZCoR_400x400.jpg"> 
+                <div class="naam2"><br>Sigrid Kaag<br><br><h2 class="info2">Demissionair minister<br> van Financiën en <br>Demissionair<br> Vicepremier</h2></div>
+            </div>
         </a>
-        <div class="lid2"></div>
-        <div class="lid3"></div>
-        <div class="lid4"></div>
+        <a class="lid2" href="https://d66.nl/mensen/franc-weerwind/">
+            <div class="lid">
+                <img class="francw" src="images/franc-weerwind-1.jpg">
+                <div class="naam3"><br>Franc Weerwind<br><br><h2 class="info3">Demissionair minister<br> voor Rechts-<br>bescherming	</h2></div>
+            </div>
+        </a>
+        <a class="lid3" href="https://d66.nl/mensen/robbert-dijkgraaf/">
+            <div class="lid">
+                <img class="robbertd" src="images/downloads.jpg">
+                <div class="naam4"><br>Robbert Dijkgraaft<br><br><h2 class="info4">Demissionair minister<br> van Onderwijs,<br> Cultuur en Wetenschap</h2></div>
+            </div>
+        </a>
+        <a class="lid4" href="https://d66.nl/mensen/ernst-kuipers/">
+            <div class="lid">
+                <img class="ernstk" src="images/downloads (2).jpg">
+                <div class="naam5"><br>Ernst Kuipers<br><br><h2 class="info5">Demissionair minister<br> van Volksgezondheid,<br> Welzijn en Sport</h2></div>
+            </div>
+        </a>
+        <a class="lid5" href="https://d66.nl/mensen/kajsa-ollongren/">
+            <div class="lid">
+                <img class="kajsao" src="images/downloads (1).jpg">
+                <div class="naam6"><br>Kajsa Ollongren<br><br><h2 class="info6"> Demissionair minister<br> van Defensie	</h2></div>
+            </div>
+        </a>
+        <a class="lid6" href="https://d66.nl/mensen/alexandra-van-huffelen/">
+            <div class="lid">
+                <img class="alexandrah" src="images/downloads (3).jpg">
+                <div class="naam7"><br>Alexandra van Huffelen<br><br><h2 class="info7">Demissionair Staatssecretaris<br> Koninkrijksrelaties en<br> Digitalisering	</h2></div>
+            </div>
+        </a>
+        <a class="lid7" href="https://d66.nl/mensen/steven-van-weyenberg/">
+            <div class="lid">
+                <img class="stevenw" src="images/pZ1H16j8_400x400.jpg">
+                <div class="naam8"><br>Steven van Weyenberg<br><br><h2 class="info8">Demissionair staatssecretaris<br> Cultuur en Media	</h2></div>
+            </div>
+</a>
     </div>
             
 </main>
